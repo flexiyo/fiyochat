54e9fef21e71c5ec397fd59c962c41d900033e14
Kaushal Krishna <kaushalkrishna011@gmail.com>
2024-11-28 17:22:19 +0530

    Debugged connection errors

--- src/app.js
+++ src/app.js
@@ -51,8 +51,7 @@ app.use((req, res, next) => {
     );
 
     if (req.method === "OPTIONS") {
-      res.sendStatus(200);
-      next();
+      res.sendStatus(204);
     } else {
       next();
     }
