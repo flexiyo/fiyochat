bc0f66631b5e6edd1c67a71d3f894f4f8b4d2cb1
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-03-04 17:46:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,32 +2,20 @@ import express from "express";
 import http from "http";
 import dotenv from "dotenv";
 import { Server } from "socket.io";
-import { setupSocketHandlers } from "./controllers/socket.controller.js";
 import { ApiResponse } from "./utils/ApiResponse.js";
 import roomRouter from "./routes/room.routes.js";
+import { setupSocketHandlers } from "./controllers/socket.controller.js";
 
 dotenv.config();
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS;
-
-/** Configurations */
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
 
-/** Middlewares */
-app.use(express.json({ limit: process.env.JSON_LIMIT || "16kb" }));
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: process.env.JSON_LIMIT || "16kb",
-  })
-);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS;
 
 app.use((req, res, next) => {
   const origin = req.headers?.origin || req.headers?.app_origin;
@@ -38,7 +26,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
       "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, app_origin fiyoat, fiyort, fiyodid"
+      "Content-Type, Authorization, app_origin, fiyoat, fiyort, fiyodid"
     );
     res.header(
       "Access-Control-Allow-Methods",
@@ -55,15 +43,12 @@ app.use((req, res, next) => {
   } else {
     res
       .status(403)
-      .json(new ApiResponse(403, null, "Forbidden: Access Denied"));
+      .json(new ApiResponse(403, null, "Forbidden: Access denied"));
   }
 });
 
-/** API Routes */
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json(new ApiResponse(200, null, "Flexiyo Chat Server is live..."));
+app.get("/api", (req, res) => {
+  res.status(200).json(new ApiResponse(200, null, "Flexiyo Auth API is live"));
 });
 
 app.get("/api/v1", (req, res) => {
@@ -72,7 +57,6 @@ app.get("/api/v1", (req, res) => {
 
 app.use("/api/v1/rooms", roomRouter);
 
-/** Setup WebSocket Handlers */
 setupSocketHandlers(io);
 
 export { server };
