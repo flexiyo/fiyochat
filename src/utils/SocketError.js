c55a67b6de9d25f6a8b1faaddf93627843dea988
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-03-04 16:56:45 +0530

    Update SocketError.js

--- src/utils/SocketError.js
+++ src/utils/SocketError.js
@@ -2,10 +2,7 @@ export const SocketError = (
   socket,
   error,
   event,
-  message = "Something went wrong.",
-  disconnectFlag = false
 ) => {
   console.error(`Error in ${event}.`, error);
-  socket.emit("error", { event, error: { message } });
-  if (disconnectFlag) socket.disconnect();
+  socket.emit("error", { event, error: { "Something went wrong." } });
 };
